77807bf9e14f080e2d8d1be58624bb916701bede
Jacob Pollard <jacobpollard2915@gmail.com>
2025-11-10 15:58:25 +0000

    Plots allegedly finished. Need to change 'chemistry' to 'Amino Acid Type'.

--- R scripts/sequence_logos.R
+++ R scripts/sequence_logos.R
@@ -14,13 +14,71 @@ alm_post <- readAAStringSet('/Users/jacobpollard/Documents/Uni/Biology/Second ye
 
 start_pos <- 137
 end_pos <- 220
+start_pos2 <- 148
+end_pos2 <- 231
 
 # Function to crop sequences
 crop_pre <- subseq(alm_pre, start = start_pos, end = end_pos)
-crop_post <- subseq(alm_post, start = start_pos, end = end_pos)
+crop_post <- subseq(alm_post, start = start_pos2, end = end_pos2)
 
-# Convert cropped sequences to character vectors
+# Create position weight matrix
 
-pre <- paste(crop_pre, collapse = "")
-post <- paste(crop_post, collapse = "")
+pwm_pre <- consensusMatrix(crop_pre, as.prob=TRUE)
+pwm_post <- consensusMatrix(crop_post, as.prob=TRUE)
 
+# Making the x axis
+
+vec <- seq(start_pos, end_pos)
+
+# Plot sequence logos
+
+logo_pre <- ggseqlogo(pwm_pre, method = 'bits') +
+  ggtitle('Pre-pandemic') +
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 16),
+        axis.text.x = element_text(size = 5,
+                                   angle = 90),
+        axis.title.x = element_blank(),
+        legend.position = "none") +
+  ylab('Bits') +
+  scale_x_continuous(
+    breaks = seq(1, length(vec), by = 1),
+    labels = seq(start_pos, end_pos, by = 1)
+  )
+
+logo_post <- ggseqlogo(pwm_post, method = 'bits') +
+  ggtitle('Post-pandemic') +
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 16),
+        axis.text.x = element_text(size = 5,
+                                   angle = 90)) +
+  ylab('Bits') +
+  scale_x_continuous(
+    breaks = seq(1, length(vec), by = 1),
+    labels = seq(start_pos2, end_pos2, by = 1)
+  )
+
+# Combine plots
+
+combined_logos <- logo_pre / logo_post +
+  plot_annotation(
+    title = 'Sequence Logos of Influenza Haemagglutinin',
+    subtitle = 'Comparing Sequences of Sialic Acid Binding Region in H1N1 
+    Haemagglutinin Before and After the 2009 Pandemic',
+    caption = 'Data Source: NCBI Influenza Virus Resource',
+    theme = theme(
+      plot.title = element_text(
+        size = 22, face = "bold", hjust = 0.5, vjust = 1
+      ),
+      plot.subtitle = element_text(
+        size = 14, face = "bold", hjust = 0.5, vjust = 1
+      ),
+      plot.caption = element_text(size = 10, hjust = 0.5)
+    )
+  )
+
+combined_logos
+
+# Save combined plot
+
+ggsave('Figures/sequence_logos.png', combined_logos, width = 15, height = 5, dpi = 300)
