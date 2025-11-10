938c32ecbaa934952e2531dd687f17ce8ebf00e1
Jacob Pollard <jacobpollard2915@gmail.com>
2025-11-10 10:34:22 +0000

    Convert cropped seqs to character vectors

--- R scripts/sequence_logos.R
+++ R scripts/sequence_logos.R
@@ -7,8 +7,8 @@ library(msa)
 
 # Load aligned MAFFT seqs
 
-pre <- readAAStringSet('/Users/jacobpollard/Documents/Uni/Biology/Second year/Sem 1/BABS/Poster/comp_bio_poster/FASTAs/alignments/mafft_pre-2008_swine_NA.fasta')
-post <- readAAStringSet('/Users/jacobpollard/Documents/Uni/Biology/Second year/Sem 1/BABS/Poster/comp_bio_poster/FASTAs/alignments/mafft_post-2008_human_NA.fasta')
+alm_pre <- readAAStringSet('/Users/jacobpollard/Documents/Uni/Biology/Second year/Sem 1/BABS/Poster/comp_bio_poster/FASTAs/alignments/mafft_pre-2008_swine_NA.fasta')
+alm_post <- readAAStringSet('/Users/jacobpollard/Documents/Uni/Biology/Second year/Sem 1/BABS/Poster/comp_bio_poster/FASTAs/alignments/mafft_post-2008_human_NA.fasta')
 
 # Define sialic acid binding region
 
@@ -16,10 +16,11 @@ start_pos <- 137
 end_pos <- 220
 
 # Function to crop sequences
+crop_pre <- subseq(alm_pre, start = start_pos, end = end_pos)
+crop_post <- subseq(alm_post, start = start_pos, end = end_pos)
 
-crop_pre <- subseq(pre, start = start_pos, end = end_pos)
-crop_post <- subseq(post, start = start_pos, end = end_pos)
+# Convert cropped sequences to character vectors
 
-#
-
+pre <- paste(crop_pre, collapse = "")
+post <- paste(crop_post, collapse = "")
 
